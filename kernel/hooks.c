452d64cf741f26148b33dcb71db99db25489ffa9
Pogging <dylstructor@gmail.com>
2022-05-05 21:43:08 -0500

    Hopefully this works.

---
+++ kernel/hooks.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "string.h"
+#include <stdarg.h>
+#include "led.c"
+#ifndef p_printf 
+#define p_printf p_printf
+int p_printf(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+    int ret = vprintf(format, args);
+    va_end(args);
+    toggle_led();
+    return ret;
+}
+#endif
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,93 +1,28 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "string.h"
-uint8_t major = 0;
-uint8_t minor = 1;
-int BACKSPACE_CHAR = 127;
-int is_eq(char *a, char *b) {
-    return strcmp(a, b) == 0;
-}
-int cls(){
-    printf("\033[2J\033[H");
-}
-int parse_command(char shell_buffer[]) {
-    char *command_array[512];
-    int i = 0;
-    char *token = strtok(shell_buffer, " ");
-    while (token != NULL) {
-        command_array[i] = token;
-        i++;
-        token = strtok(NULL, " ");
-    }
-    command_array[i] = NULL;
-    if (is_eq(command_array[0], "cls")) {
-        cls();
-    }
-    else if (is_eq(command_array[0], "set_pin")){
-        const uint pin = atoi(command_array[1]);
-        //Convert arg 1 to bool
-        const bool state = atoi(command_array[2]);
-        gpio_init(pin);
-        gpio_set_dir(pin, GPIO_OUT);
-        gpio_put(pin, state);
-        printf("set led to %d\n", state);
-    }
-}
-int shell_init() {
-    char shell_buffer[256];
-    char character;
-    int ascii;
-    int i = 0;
-    printf("$ ");
-    while (true){
-    //Check if the shell buffer has overflowed
-    if (i == 256) {
-        printf("\nShell buffer overflow!\n");
-        for (int j = 0; j < 256; j++) {
-            shell_buffer[j] = '\0';
-            i = 0;
-        }
-    }
-    //Get one character at a time
-    character = getchar();
-    //Turn character into an int
-    ascii = character;
-    shell_buffer[i] = character;
-    putchar(shell_buffer[i]);
-    i++;
-    //Check if the input is a carriage return
-    if (shell_buffer[i-1] == '\r') {
-        shell_buffer[i-1] = '\0';
-        i = 0;
-        printf("\n");
-        parse_command(shell_buffer);
-        for (int j = 0; j < 256; j++){
-            shell_buffer[j] = '\0';
-        }
-        printf("$ ");
-    }
-    if (ascii == BACKSPACE_CHAR) {
-        if (i-1 < 1) {
-            i--;
-            printf("%c", ascii);
-            continue;
-        }
-        shell_buffer[i-1] = '\0';
-        i--;
-        i--;
-        printf("\b \b");
-    }
-}}
-int main() {
+#include "hooks.c"
+#include "led.c"
+#include "shell.c"
 
+#ifndef major
+#define major major
+uint8_t major = 0;
+#endif
+
+#ifndef minor
+#define minor minor
+uint8_t minor = 3;
+#endif
+
+int main() {
+    toggle_led();
     stdio_init_all();
-    printf("PogOS Initializing...\n");
+    p_printf("PogOS Initializing...\n");
     sleep_ms(1000);
-    printf("PogOS v%d.%d\n", major, minor);
-    printf("PogOS is ready!\n");
-    //Clear the screen
-    cls();
+    p_printf("PogOS v%d.%d\n", major, minor);
+    p_printf("PogOS is ready!\n");
     //Start shell
+    toggle_led();
     shell_init();
     return 0;
 }
