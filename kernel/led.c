a9456f228c1cbac2a1365c12dbfbdc6c22ec8172
Pogging <dylstructor@gmail.com>
2022-05-25 17:55:10 -0500

    pragma once

--- kernel/led.c
+++ kernel/led.c
@@ -1,14 +1,9 @@
 #include "pico/stdlib.h"
-#ifndef LED_state
-#define LED_state LED_state
+#pragma once
 bool LED_state = false;
-#endif
-#ifndef toggle_led
-#define toggle_led toggle_led
 void toggle_led() {
     LED_state = !LED_state;
     gpio_init(PICO_DEFAULT_LED_PIN);
     gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
     gpio_put(PICO_DEFAULT_LED_PIN, LED_state);
 } 
-#endif
