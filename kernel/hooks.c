a2ec8d6a9bfcd88a2178f388306e83786989b3c2
Pogging <dylstructor@gmail.com>
2022-05-25 17:55:10 -0500

    pragma once

--- kernel/hooks.c
+++ kernel/hooks.c
@@ -1,10 +1,9 @@
+#pragma once
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "string.h"
 #include <stdarg.h>
 #include "led.c"
-#ifndef p_printf 
-#define p_printf p_printf
 int p_printf(const char *format, ...) {
     va_list args;
     va_start(args, format);
@@ -14,4 +13,3 @@ int p_printf(const char *format, ...) {
     //End Extra Code
     return ret;
 }
-#endif
