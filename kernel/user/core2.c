c59dacdc3aadd41ce78fa6aa8ef57faf09bfc639
Stigl <realstigl@gmail.com>
2023-06-20 05:31:56 +0000

    Fix the led blinking

--- kernel/user/core2.c
+++ kernel/user/core2.c
@@ -23,10 +23,8 @@ void core2_main() {
             }
         }
         if (led_to_blink != 0){
-         gpio_init(led_to_blink);
-        gpio_set_dir(led_to_blink, GPIO_OUT);
-        gpio_put(led_to_blink, led_rep_status);
-        led_rep_status = !led_rep_status;
+            gpio_put(led_to_blink, led_rep_status);
+            led_rep_status = !led_rep_status;
         }
         
         sleep_ms(500);
